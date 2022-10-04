463bc7136ad666df0188e15d6593ea394f59b853
Vitor Mamede <mamede.vitor@gmail.com>
2022-10-03 21:28:02 -0300

    Criação de telas

--- main.py
+++ main.py
@@ -1,3 +1,55 @@
-import requests as r
-print ('Teste')
+from PySide6 import QtCore
+from PySide6.QtCore import QCoreApplication
+from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import (QApplication, QMainWindow, QMessageBox, QTableWidgetItem, QWidget)
+from ui_main import Ui_MainWindow
+import sys
 import pandas as pd
+
+class MainWindow(QMainWindow, Ui_MainWindow):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        self.setupUi(self)
+        self.setWindowTitle("Sistema de Gerenciamento")
+        appIcon = QIcon(u"")
+        self.setWindowIcon(appIcon)
+
+
+        #################################
+        #TOGGLE BUTTON
+        self.btn_toggle.clicked.connect(self.leftContainer)
+        ###################################################
+
+        #############################################################################################
+        #Páginas do Sistema
+        self.btn_home.clicked.connect(lambda: self.Pages.setCurrentWidget(self.pg_home))
+        self.btn_cadastrar.clicked.connect(lambda: self.Pages.setCurrentWidget(self.pg_cadastrar))
+        self.btn_sobre.clicked.connect(lambda: self.Pages.setCurrentWidget(self.pg_sobre))
+        self.btn_pg_cadastro.clicked.connect(lambda: self.Pages.setCurrentWidget(self.pg_cadastro))
+        #############################################################################################
+
+        ###########################################################
+
+        ############################################################
+    def leftContainer(self):
+        
+        width = self.left_container.width()
+
+        if width == 9:
+            newWidth = 200
+        else:
+            newWidth = 9
+
+        self.animation = QtCore.QPropertyAnimation(self.left_container, b"maximumWidth")
+        self.animation.setDuration(500)
+        self.animation.setStartValue(width)
+        self.animation.setEndValue(newWidth)
+        self.animation.setEasingCurve(QtCore.QEasingCurve.InOutQuart)
+        self.animation.start()
+
+if __name__ == "__main__":
+    
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
